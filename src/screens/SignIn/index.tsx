22b3f567460dfc6d03c7f179ca9d6ffbdaf081b7
Eliel Martins <elieuprata@gmail.com>
2023-05-04 09:57:56 -0300

    add assets files and initial configuration

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import { styles } from './styles'
+
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  )
+}
