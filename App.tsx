9974790c3ff58285fb04083db404f5f73e2d4fda
Eliel Martins <elieuprata@gmail.com>
2023-05-06 11:46:14 -0300

    install and configure realmeDB dependency

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components/native'
+import { AppProvider, UserProvider } from '@realm/react'
 import {
   useFonts,
   Roboto_400Regular,
@@ -7,9 +8,12 @@ import {
 
 import theme from './src/theme'
 
+import { REALME_APP_ID } from '@env'
+
 import { SignIn } from './src/screens/SignIn'
 import { Loading } from './src/components/Loading'
 import { StatusBar } from 'react-native'
+import { Home } from './src/screens/Home'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -19,13 +23,17 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALME_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   )
 }
--- src/@types/env.d.ts
+++ src/@types/env.d.ts
@@ -1,4 +1,5 @@
 declare module '@env' {
+  export const REALME_APP_ID: string
   export const ANDROID_CLIENT_ID: string
   export const IOS_CLIENT_ID: string
 }
