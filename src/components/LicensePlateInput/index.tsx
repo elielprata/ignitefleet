46f347bab631ce4fc5de607c5b18043075008f6b
Eliel Martins <elieuprata@gmail.com>
2023-05-13 15:38:28 -0300

    add reference in LicensePlateInput component

--- src/components/LicensePlateInput/index.tsx
+++ src/components/LicensePlateInput/index.tsx
@@ -1,24 +1,32 @@
+import { forwardRef } from 'react'
+import { TextInput, TextInputProps } from 'react-native'
+
 import { useTheme } from 'styled-components'
+
 import { Container, Input, Label } from './styles'
-import { TextInputProps } from 'react-native'
 
 type Props = TextInputProps & {
   label: string
 }
 
-export function LicensePlateInput({ label, ...rest }: Props) {
-  const { COLORS } = useTheme()
+const LicensePlateInput = forwardRef<TextInput, Props>(
+  ({ label, ...rest }, ref) => {
+    const { COLORS } = useTheme()
 
-  return (
-    <Container>
-      <Label>{label}</Label>
+    return (
+      <Container>
+        <Label>{label}</Label>
 
-      <Input
-        maxLength={7}
-        autoCapitalize="characters"
-        placeholderTextColor={COLORS.GRAY_400}
-        {...rest}
-      />
-    </Container>
-  )
-}
+        <Input
+          ref={ref}
+          maxLength={7}
+          autoCapitalize="characters"
+          placeholderTextColor={COLORS.GRAY_400}
+          {...rest}
+        />
+      </Container>
+    )
+  }
+)
+
+export { LicensePlateInput }
