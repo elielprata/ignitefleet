2d2bed3268a07fc65b54ed86a4bf28e72bd00f82
Eliel Martins <elieuprata@gmail.com>
2023-05-13 12:23:21 -0300

    create Historic schema

---
+++ src/libs/realm/schemas/Historic.ts
@@ -0,0 +1,46 @@
+import { Realm } from '@realm/react'
+
+type GenerateProps = {
+  user_id: string
+  description: string
+  licensed_plate: string
+}
+
+export class Historic extends Realm.Object<Historic> {
+  _id!: string
+  user_id!: string
+  licensed_plate!: string
+  status!: string
+  created_at!: string
+  updated_at!: string
+
+  static generate({ user_id, licensed_plate, description }: GenerateProps) {
+    return {
+      _id: new Realm.BSON.UUID(),
+      user_id,
+      licensed_plate,
+      description,
+      status: 'departure',
+      created_at: new Date(),
+      updated_at: new Date(),
+    }
+  }
+
+  static schema = {
+    name: 'Historic',
+    primaryKey: '_id',
+
+    properties: {
+      _id: 'uuid',
+      user_id: {
+        type: 'string',
+        indexed: true,
+      },
+      license_plate: 'string',
+      description: 'string',
+      status: 'string',
+      created_at: 'date',
+      updated_at: 'date',
+    },
+  }
+}
